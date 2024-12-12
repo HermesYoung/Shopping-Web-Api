5014f39f9f992d949bbf7eb0568138746f607a36
HermesYoung <tails777tw@gmail.com>
2024-12-13 07:23:52 +0800

    extract class to another file

---
+++ src/Repositories/Common/ErrorBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Repositories.Common;
+
+public abstract class ErrorBase(string message, object? data)
+{
+    public string Message { get; } = message;
+    protected object? Data { get; } = data;
+}
+
+public abstract class ErrorBase<TData>(string message, TData data) : ErrorBase(message, data)
+{
+    public TData? ErrorData => (TData?)Data;
+}
--- src/Repositories/Common/Result.cs
+++ src/Repositories/Common/Result.cs
@@ -50,13 +50,3 @@ public struct Result
     public static Result Failure() => new Result(false, null);
 }
 
-public abstract class ErrorBase(string message, object? data)
-{
-    public string Message { get; } = message;
-    public object? Data { get; } = data;
-}
-
-public abstract class ErrorBase<TData>(string message, TData data) : ErrorBase(message, data)
-{
-    public TData? ErrorData => (TData?)Data;
-}
