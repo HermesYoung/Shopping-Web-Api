eaaffcc7718d2e97a829368a893f362d0f0c416b
HermesYoung <tails777tw@gmail.com>
2024-12-17 16:26:03 +0800

    add logging

--- src/ManagementSite/Program.cs
+++ src/ManagementSite/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.HttpLogging;
 using Repositories.Extensions;
+using Serilog;
+using Serilog.Events;
 
 namespace ManagementSite;
 
@@ -9,8 +12,21 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
+        const string template = "{Timestamp:HH:mm:ss} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}";
+        var logger = new LoggerConfiguration().WriteTo.Console(outputTemplate: template)
+            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.StaticFiles.StaticFileMiddleware", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Routing.EndpointMiddleware", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Mvc.Infrastructure.ObjectResultExecutor", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Mvc.Infrastructure.ControllerActionInvoker",
+                LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .CreateLogger();
         builder.Services.AddControllers();
+        builder.Services.AddSerilog(logger);
+        builder.Services.AddHttpLogging(options =>
+            options.LoggingFields = HttpLoggingFields.Request | HttpLoggingFields.Response);
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -20,11 +36,12 @@ public class Program
         {
             throw new Exception("No connection string found");
         }
-        
+
         builder.Services.AddDbContext(connectionString);
         builder.Services.AddRepositories();
 
         var app = builder.Build();
+
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -35,9 +52,10 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseHttpLogging();
+
         app.UseAuthorization();
-
-
+        
         app.MapControllers();
 
         app.Run();
